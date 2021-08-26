7ce90239b1a16f6380ad28f3fae9faa471bf0617
miranlee <rannan09@naver.com>
2021-08-26 17:41:15 +0900

    johnny-five

---
+++ johnny-five_test/main_dht.js
@@ -0,0 +1,17 @@
+const { Board, Led } = require("johnny-five");
+const board = new Board({
+    port:'COM5'
+});
+
+board.on("ready", () => {
+  const led = new Led(13);
+
+  // This will grant access to the led instance
+  // from within the REPL that's created when
+  // running this program.
+  board.repl.inject({
+    led
+  });
+
+  led.blink();
+});
